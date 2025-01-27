f89eb1decaa826fe05e2ae5e471b65d0d21584ae
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-27 16:42:51 +0900

    remove virtural from toString

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -12,7 +12,7 @@ class ClapTrap {
         ClapTrap& operator=(const ClapTrap& other);
         friend std::ostream& operator<<(std::ostream& os, const ClapTrap& clapTrap);
         ~ClapTrap();
-        virtual std::string toString() const;
+        std::string toString() const;
         virtual std::string getClassName() const;
         const std::string& getName() const;
         unsigned int getHitPoints() const;
