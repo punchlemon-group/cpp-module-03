a4d7ac481b4c6d66ff3c122d0fcd8c4e5e9d9ebd
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-29 12:19:12 +0900

    update ex02

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef __FRAGTRAP_HPP__
+#define __FRAGTRAP_HPP__
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+public:
+    /* constructor */
+    FragTrap();
+    FragTrap(const std::string& name);
+    FragTrap(const FragTrap& copy);
+    FragTrap& operator=(const FragTrap& copy);
+
+    /* destructor */
+    virtual ~FragTrap();
+
+    /* general */
+    void highFiveGuys();
+
+protected:
+    /* override */
+    virtual void _init();
+    virtual std::string _getClassName() const;
+};
+
+#endif /* __FRAGTRAP_HPP__ */
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,14 +1,14 @@
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main() {
-    ScavTrap zero("SC4V-TP_0");
+    FragTrap zero("FR4G-TP_0");
     zero.takeDamage(4);
     zero.beRepaired(2);
     zero.takeDamage(10);
     zero.takeDamage(15);
     zero.beRepaired(20);
     zero.takeDamage(15);
-    zero.guardGate();
+    zero.highFiveGuys();
     zero.takeDamage(15);
     zero.beRepaired(20);
     zero.beRepaired(20);
@@ -16,8 +16,8 @@ int main() {
     zero.beRepaired(20);
     zero.beRepaired(20);
     zero.beRepaired(20);
-    zero.attack("SC4V-TP_2");
-    zero.attack("SC4V-TP_3");
-    zero.attack("SC4V-TP_4");
-    zero.guardGate();
+    zero.attack("FR4G-TP_2");
+    zero.attack("FR4G-TP_3");
+    zero.attack("FR4G-TP_4");
+    zero.highFiveGuys();
 }
